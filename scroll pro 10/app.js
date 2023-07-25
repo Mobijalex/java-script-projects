01f6c79af05e2fb53e4c303edbe4cfecc4382f5e
mobijalex <mobijalex6@gmail.com>
2023-07-25 22:22:44 +0530

    scroll pro done

--- scroll pro 10/app.js
+++ scroll pro 10/app.js
@@ -16,9 +16,61 @@ navToggle.addEventListener("click", function () {
   //   linksContainer.classList.toggle("show-links");
   const containerHeight = linksContainer.getBoundingClientRect();
   const linksHeight = links.getBoundingClientRect().height;
-  console.log(linksHeight);
+  if (containerHeight === 0) {
+    linksContainer.Style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.Style.height = 0;
+  }
 });
+const navbar = document.getElementById("nav");
+const toplink = document.querySelector(".top-link");
 // ********** fixed navbar ************
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  if (scrollHeight > 500) {
+    toplink.classList.add("show-link");
+  } else {
+    toplink.classList.remove("show-link");
+  }
+});
 
 // ********** smooth scroll ************
 // select links
+
+const scrollLinks = document.querySelectorAll(".scroll-links");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    linksContainer.style.height = 0;
+  });
+});
+// calculate heights
